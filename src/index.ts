73c869c442da9409d8e6c542281540ec7539b234
Rafael <rafaelhayashi8@gmail.com>
2023-05-09 15:50:08 -0300

    feat: criacao de metodos para leitura de bitcoin em tempo real, model de candle

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { config } from 'dotenv';
+import Period from './enums/Period';
+import Candle from './models/Candle';
+config();
+
+const readMarketPrice = async (): Promise<number> => {
+    const result = await axios.get(process.env.PRICES_API);
+    const data = result.data;
+    const price = data.bitcoin.usd;
+    console.log(price)
+    return price;
+}
+
+const generateCandles = async () => {
+    while (true) {
+        const loopTimes = Period.ONE_MINUTE / Period.TEN_SECONDS
+        const candle = new Candle('BTC')
+
+        console.log('--------------------')
+        console.log('Generating new candle')
+
+        for (let i = 0; i < loopTimes; i++) {
+            const price = await readMarketPrice()
+            candle.addValue(price)
+            console.log(`Market price #${i + 1} of ${loopTimes}`)
+            await new Promise(r => setTimeout(r, Period.TEN_SECONDS))
+        }
+
+        candle.closeCandle()
+        console.log('Candle close')
+        console.log(candle.toSimpleObject())
+    }
+}
+
+generateCandles()
---
+++ src/messages/messageChannel.ts
@@ -0,0 +1,2 @@
+import { config } from 'dotenv';
+import { Channel, connect } from 'amqplib'
