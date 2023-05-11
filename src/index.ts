80f614f1d0db3a657b0a0c6c742d589da8cdbf20
Rafael <rafaelhayashi8@gmail.com>
2023-05-11 11:43:23 -0300

    build

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const generateCandles = async () => {
     const messageChannel = await createMessageChannel()
     if (messageChannel) {
         while (true) {
-            const loopTimes = Period.ONE_MINUTE / Period.TEN_SECONDS
+            const loopTimes = Period.FIVE_MINUTES / Period.TEN_SECONDS
             const candle = new Candle('BTC')
 
             console.log('--------------------')
